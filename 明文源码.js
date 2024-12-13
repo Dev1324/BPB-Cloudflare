b2bc85c5113889f25aa366cb4a2c1abf8fdf854a
Dev13245 <149612258+Dev1324@users.noreply.github.com>
2024-12-13 23:55:39 +0330

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -9465,12 +9465,12 @@ export { worker_default as default };
 
 
 /*! Bundled license information:
-		js-sha256/src/sha256.js:
-		(**
-		* [js-sha256]{@link https://github.com/emn178/js-sha256}
-		*
-		* @version 0.11.0
-		* @author Chen, Yi-Cyuan [emn178@gmail.com]
+	js-sha256/src/sha256.js:
+	(**
+	* [js-sha256]{@link https://github.com/emn178/js-sha256}
+	*
+	* @version 0.11.0
+	* @author Chen, Yi-Cyuan [emn178@gmail.com]
 		* @copyright Chen, Yi-Cyuan 2014-2024
 		* @license MIT
 		*)
